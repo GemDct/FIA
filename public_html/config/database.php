ac549dc74909dab3acef9e459e7b9eca0f1390d5
GemDct <dct.adesweb@gmail.com>
2025-12-01 12:16:48 +0100

    Update database.php

--- public_html/config/database.php
+++ public_html/config/database.php
@@ -2,8 +2,8 @@
 // Config for database connection
 return [
     'host' => 'localhost',
-    'dbname' => 'codemotard_db',
-    'user' => 'codemotard_user',
-    'password' => 'codemotard_password',
+    'dbname' => 'u607876221_facturiabdd',
+    'user' => 'u607876221_userfia',
+    'password' => 'hgd8$ML>h7jht',
     'charset' => 'utf8mb4'
 ];
